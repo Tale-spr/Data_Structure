9f2c9386409ca62ee84f75879f7b0efb5962684a
ArkRoyal1941 <92839933+ArkRoyal1941@users.noreply.github.com>
2025-03-25 20:53:59 +0800

    Update prim.cpp

--- prim.cpp
+++ prim.cpp
@@ -1,3 +1,5 @@
+//UPD3.25
+//可以跑出整个图的最小生成树的所有边，用于制作导览图
 #include<iostream>
 #include<cstdio>
 #include<queue>
@@ -17,9 +19,9 @@ const int maxm=4e5+5,maxn=5005;
 struct edge{
     int to,nxt,dis;
 }e[maxm];
-int hd[maxn],dis[maxn],vis[maxm],cnt,n,tot,ans=0,now=1;
-// 添加一条边，链式前向星
-// 添加一条边，u为起点，v为终点，w为权重
+int hd[maxn],dis[maxn],vis[maxm],cnt,n,m,tot,ans=0,now=1;
+pair<int, int> parent[maxn];
+//链式前向星存图
 void add(int u,int v,int w)
 {
     cnt++;
@@ -28,29 +30,35 @@ void add(int u,int v,int w)
     e[cnt].nxt=hd[u];
     hd[u]=cnt;
 }
+
 void prim()
 {
-    // 遍历第一个节点的所有邻接边，更新dis数组
+    // 遍历第一个节点的所有邻接边，更新距离和父节点
     for(int i=hd[1];i;i=e[i].nxt)
-    dis[e[i].to]=min(dis[e[i].to],e[i].dis);
-    // 当tot小于n-1时，继续循环
+    {
+        dis[e[i].to]=min(dis[e[i].to],e[i].dis);
+        parent[e[i].to]={1,e[i].dis};
+    }
     while(tot<n-1)
     {
-        // 初始化mini为最大值，rec为当前节点
         int mini=0x7fffffff,rec=now;
-        // tot加1
         tot++;
-        // 标记当前节点为已访问
         vis[now]=1;
-        // 遍历所有节点，找到未访问的节点中dis最小的节点
+        // 遍历所有节点，找到距离最小的节点
         for(int i=1;i<=n;i++)
         if(!vis[i]&&mini>dis[i])mini=dis[i],now=i;
-        // 如果rec等于now，说明已经无法找到未访问的节点，退出循环
+        // 如果没有找到距离最小的节点，说明图不连通，退出循环
         if(rec==now)tot=n+1;
-        // 将mini加到ans中
         ans+=mini;
+        // 输出当前节点和父节点
+        cout<<parent[now].first<<" "<<now<<" "<<parent[now].second<<'\n';
+        // 遍历当前节点的所有邻接边，更新距离和父节点
         for(int i=hd[now];i;i=e[i].nxt)
-        if(e[i].dis<dis[e[i].to]&&!vis[e[i].to])dis[e[i].to]=e[i].dis;
+        if(e[i].dis<dis[e[i].to]&&!vis[e[i].to])
+        {
+            dis[e[i].to]=e[i].dis;
+            parent[e[i].to]={now, e[i].dis};
+        }
     }
 }
 int u,v,w;
@@ -64,10 +72,17 @@ int main()
             if(w>0)
             add(i,j,w),add(j,i,w);
         }
-    for(int i=2;i<=n;i++)dis[i]=0x7fffffff;
+    for(int i=2;i<=n;i++) dis[i]=0x7fffffff;
     prim();
-    // cout<<tot<<'\n';
-    if(tot==n-1)return cout<<ans,0;
-    cout<<"此图不联通";
+    if(tot==n-1) cout<<"Total Weight: "<<ans<<'\n';
+    else cout<<"此图不联通";
+    
     return 0;
 }
+/*
+4
+0 2 2 3
+2 0 5 1
+2 5 0 3
+3 1 3 0
+*/
